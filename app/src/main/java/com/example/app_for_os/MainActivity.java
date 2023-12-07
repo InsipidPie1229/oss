249af11fc7be80841d4787cacc3a2d51c4ace8d7
InsipidPie1229 <ghdwnsaos@gmail.com>
2023-12-07 17:05:37 +0900

    main java code for catbutton

--- app/src/main/java/com/example/app_for_os/MainActivity.java
+++ app/src/main/java/com/example/app_for_os/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.app_for_os;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button catBtn;
+        ImageView catImg;
+
+        catBtn = (Button) findViewById(R.id.catBtn);
+        catImg = (ImageView) findViewById(R.id.catImg);
+
+        catBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                catImg.setVisibility(View.VISIBLE);
+                catImg.setImageResource(R.drawable.cat);
+
+            }
+        });
+
+
     }
 }
