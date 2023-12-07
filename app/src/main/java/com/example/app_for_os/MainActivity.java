07d2d3e0fcaeff24609faf246992ba8c0d370860
InsipidPie1229 <ghdwnsaos@gmail.com>
2023-12-07 18:27:20 +0900

    fix main xml, java for emergency_call

--- app/src/main/java/com/example/app_for_os/MainActivity.java
+++ app/src/main/java/com/example/app_for_os/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.app_for_os;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,10 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button catBtn;
+        Button catBtn, emrBtn;
         ImageView catImg;
 
         catBtn = (Button) findViewById(R.id.catBtn);
+        emrBtn = (Button) findViewById(R.id.emrBtn);
         catImg = (ImageView) findViewById(R.id.catImg);
 
         catBtn.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +32,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        emrBtn.setOnClickListener(new View.OnClickListener(){
 
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), emergency_call.class);
+                startActivity(intent);
+            }
+        });
     }
 }
